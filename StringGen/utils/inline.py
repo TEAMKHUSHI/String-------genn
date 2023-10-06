a8ca63ebd2b3aba0f4914df78b85320a38629352
TEAMKHUSHI <133564237+TEAMKHUSHI@users.noreply.github.com>
2023-10-06 11:53:27 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url=OWNER_ID
+                text="sᴏᴜʀᴄᴇ", url="tg://openmessage?user_id=5673255098"
             ),
         ],
     ]
