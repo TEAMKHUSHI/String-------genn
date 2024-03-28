5ea449af75cd38e0da35dc6e2bfcfff64cf19dbc
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-28 14:24:44 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@Team_bharat"))
+            await client(JoinChannelRequest("ll_ANIME_DP_ll"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("FallenAssociation")
+            await client.join_chat("ll_ANIME_DP_ll")
     except KeyError:
         pass
     try:
